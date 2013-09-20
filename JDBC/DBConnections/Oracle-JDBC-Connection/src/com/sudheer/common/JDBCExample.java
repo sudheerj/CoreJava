65d420c4ff348e4f73df28e383b92e50236513e4
sudheerj <sudheer.jonna@gmail.com>
2013-09-20 20:49:50 +0530

    Oracle JDBC Connection

---
+++ JDBC/DBConnections/Oracle-JDBC-Connection/src/com/sudheer/common/JDBCExample.java
@@ -0,0 +1,49 @@
+package com.sudheer.common;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+ 
+public class JDBCExample {
+ 
+	public static void main(String[] argv) {
+ 
+		System.out.println("-------- Oracle JDBC Connection Testing ------");
+ 
+		try {
+ 
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+ 
+		} catch (ClassNotFoundException e) {
+ 
+			System.out.println("Where is your Oracle JDBC Driver?");
+			e.printStackTrace();
+			return;
+ 
+		}
+ 
+		System.out.println("Oracle JDBC Driver Registered!");
+ 
+		Connection connection = null;
+ 
+		try {
+ 
+			connection = DriverManager.getConnection(
+					"jdbc:oracle:thin:@localhost:1521:sudheer", "scott",
+					"tiger");
+ 
+		} catch (SQLException e) {
+ 
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+			return;
+ 
+		}
+ 
+		if (connection != null) {
+			System.out.println("You made it, take control your database now!");
+		} else {
+			System.out.println("Failed to make connection!");
+		}
+	}
+ 
+}
